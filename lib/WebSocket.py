d738b7ffb4136e759568997fe478b17ab8d3e502
wilde <luke.wilde@gmail.com>
2019-02-06 21:27:11 +0000

    Sending cool data.

--- lib/WebSocket.py
+++ lib/WebSocket.py
@@ -2,10 +2,14 @@ from websocket import create_connection
 import thread
 import time
 import threading
+import json
 
 class WebSocket:
+  def __init__(self):
+    self.ws = create_connection("ws://192.168.0.13:8080")
 
-  def send(self, message):
-    ws = create_connection("ws://192.168.0.13:8080")
-    ws.send(message)
-    ws.close()
+  def send(self, data):
+    self.ws.send(json.dumps(data))
+
+  def close(self):
+    self.ws.close()
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from lib.Co2Sensor import Co2Sensor
 from lib.Lcd import Lcd
 from lib.CsvPersistance import CsvPersistance
 from lib.WebSocket import WebSocket
-
 import time
 
 co2Sensor = Co2Sensor()
@@ -10,13 +9,18 @@ lcd = Lcd()
 csvPersistance = CsvPersistance()
 webSocket = WebSocket()
 
-print("trying to communicate")
-webSocket.send('ello?')
+# webSocket.send(json.dumps({'time': 123, 'data': 123}))
 
-while False:
+
+while True:
   co2Ppm = co2Sensor.getReading()
 
   lcd.display("CO2: " + str(co2Ppm) + " PPM")
-  csvPersistance.append(str(time.time()), str(co2Ppm))
+
+  timestamp = str(time.time())
+  co2Reading = str(co2Ppm)
+
+  csvPersistance.append(timestamp, co2Reading)
+  webSocket.send({'time': timestamp, 'co2': co2Reading})
 
   time.sleep(1)
